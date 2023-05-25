0897caa229181685744d0f46f2559b7fa5f6673c
Eric Hines <hines.eric.m@gmail.com>
2023-05-25 14:35:32 -0500

    added to json.package: start, and deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+app.listen('app is listening on port: ', PORT);
 app.get('/messages', (req, res) => {
     res.sendStatus(204);
 });
